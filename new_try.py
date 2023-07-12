48d95a922de0ee215f625ec86df2091eac5b8bbd
zyx0015 <139255115+zyx0015@users.noreply.github.com>
2023-07-12 09:57:43 +0800

    Add files via upload

--- new_try.py
+++ new_try.py
@@ -11,14 +11,8 @@ st.title("PDF to PNG")
 file = st.file_uploader("请上传PDF")
 if file is not None:
     doc = fitz.open(stream=file.read(), filetype="pdf")
-    work_path = st.text_input("Enter work path")
-    folder_name = st.text_input("Enter folder name")
-    os.chdir(work_path)
-    if folder_name:
-        os.makedirs(folder_name, exist_ok=True)
-        for i in range(len(doc)):
+    for i in range(len(doc)):
             page = doc.load_page(i)
-            os.chdir(work_path)
             pix = page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72), dpi=300)
             img_array = np.frombuffer(pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, pix.n))
             st.image(img_array, caption=f"Page {i+1}", use_column_width=True)
