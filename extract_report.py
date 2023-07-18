fb48f1cef8a88fcf448d09ba662e2ccabae1ca35
zyx0015 <139255115+zyx0015@users.noreply.github.com>
2023-07-18 19:00:05 +0800

    Add files via upload

---
+++ extract_report.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import streamlit as st
+from skimage import morphology
+import fitz
+import cv2
+import zipfile
+
+def extract_report(file_name,book_title,dpi=300,contour_area_val=100000,size_val=10000):
+    final_pictures=[]
+    final_names=[]
+    doc = fitz.open(stream=file_name.read(), filetype="pdf")
+    for page_index in range(len(doc)):
+        page = doc.load_page(page_index)
+        pix = page.get_pixmap(matrix=fitz.Matrix(dpi/72, dpi/72), dpi=dpi)
+        img_array = np.frombuffer(pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, pix.n))
+        img = img_array
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        kernel = np.ones((3, 3), np.uint8)
+        dilated = cv2.dilate(thresh, kernel, iterations=2)
+        data=np.array(dilated, dtype= bool)
+        rem = morphology.remove_small_objects(data, 15000,connectivity=1)
+        binary_image = np.uint8(rem) * 255
+        kernel = np.ones((3, 3), np.uint8)
+        dilated_2 = cv2.dilate(binary_image, kernel, iterations=3)
+        mask = dilated_2.copy()
+        contours, hierarchy = cv2.findContours(dilated_2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        valid = len(contours) > 0
+        for k in range(len(contours)):
+           mask = cv2.drawContours(mask, contours, k, 255, cv2.FILLED)
+        kernel_size = (3, 3)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
+        closed_image = cv2.morphologyEx(dilated_2, cv2.MORPH_CLOSE, kernel)
+        edges = cv2.Canny(closed_image, 100, 200)
+        raw_contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours=[]
+        for i in raw_contours:
+          area=cv2.contourArea(i)
+          if area>contour_area_val:
+            contours.append(i)
+          else:
+            continue
+        for i in range(len(contours)):
+            x, y, w, h = cv2.boundingRect(contours[i])
+            raw_sample = cv2.bitwise_not(thresh[y:y+h, x:x+w])
+            sample = mask[y:y+h, x:x+w]
+            height, width = raw_sample.shape[:2]
+            if height*width>size_val:
+              final_pictures.append(raw_sample)
+              final_names.append(f'{book_title}_{page_index}_{i}')
+            else:
+              continue
+    return dict(zip(final_names,final_pictures))
+
+
+def save_arrays_to_zip(names, arrays, zip_file_path):
+    with zipfile.ZipFile(zip_file_path, mode='w') as zip_file:
+        for i in range(len(names)):
+            cv2.imwrite(f'{names[i]}.png',arrays[i])
+            zip_file.write(f'{names[i]}.png')
+            os.remove(f'{names[i]}.png')
+
+
+############start##################
+st.title("Extract information from your archaeological reports")
+
+file_name = st.file_uploader("import your pdf file")
+book_title = st.text_input("pdf label")
+
+if file_name is not None:
+    min_size = st.slider("accepted minimum contour size:",1,1000,100,5)
+    img_dict=extract_report(file_name,book_title,contour_area_val=min_size*100)
+    img_key=list(img_dict.keys())
+    img_list=list(img_dict.values())
+    for i in range(len(img_list)):
+        st.image(img_list[i], caption=f"Page {i+1}", use_column_width=False)
+    save_arrays_to_zip(img_key,img_list, 'output.zip')
+    with open('output.zip', 'rb') as f:
+      result = f.read()
+      st.download_button(
+          label='Download output.zip',
+          data=result,
+          file_name='output.zip',
+          mime='application/zip'
+      )
